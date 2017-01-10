9070393c8ee812b0acee64b0f468c4a6cf357ba1
Peter Corcoran <peter@lemondigits.com>
2017-01-10 23:12:06 +0000

    Added support for Permission, Role and User classes to be extended by projects depending on dachi-permissions. This allows attachment of extra data to these tables.

--- src/Authentication/Models/Role.php
+++ src/Authentication/Models/Role.php
@@ -6,6 +6,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @Entity(repositoryClass="RoleRepository")
+ * @InheritanceType("SINGLE_TABLE")
+ * @DiscriminatorColumn(name="type", type="string")
  * @Table(name="authentication_roles")
  */
 class Role extends Model {
--- src/Authentication/Models/User.php
+++ src/Authentication/Models/User.php
@@ -6,6 +6,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @Entity(repositoryClass="UserRepository")
+ * @InheritanceType("SINGLE_TABLE")
+ * @DiscriminatorColumn(name="type", type="string")
  * @Table(name="authentication_users")
  */
 class User extends Model {
